c7ee1177dadb58ebb38671c7941a80a34819597f
Tim <74722421+TimothyStott@users.noreply.github.com>
2023-11-08 17:50:50 -0500

    Add Page Links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
+import { usePathname } from 'next/navigation'
 
 
 export const metadata: Metadata = {
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,17 @@
+'use client'
+
 import Image from "next/image"
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 export default function Navbar() {
+    const path = usePathname()
+    console.log(path)
     return (
-        <nav className="w-full h-[100px] bg-[#F56600]">
+        <nav className="w-full h-[100px] bg-[#F56600]" style={{display:"grid", gridTemplateColumns:"3fr 2fr 3fr"}}>
             <div className="">
                 <Image
                     src='/jr2.png'
@@ -17,7 +21,7 @@ export default function Navbar() {
                 />
 
             </div>
-            
+
             <div className="">
                 <ul className="flex flex-row justify-center items-center">
                     <Link
@@ -25,33 +29,41 @@ export default function Navbar() {
                         <li className="text-white font-bold mx-4">Home</li>
                     </Link>
                     <Link
-                    href={"/about"}>
+                        href={"/about"}>
                         <li className="text-white font-bold mx-4">About</li>
                     </Link>
                     <Link
-                    href={"/visit-us"}>
+                        href={"/visit-us"}>
                         <li className="text-white font-bold mx-4">Visit Us</li>
                     </Link>
                     <Link
-                    href={"/tap-list"}>
+                        href={"/tap-list"}>
                         <li className="text-white font-bold mx-4">Tap List</li>
                     </Link>
                     <Link
-                    href={"/find-our-beers"}>
+                        href={"/find-our-beers"}>
                         <li className="text-white font-bold mx-4">Find Our Beer</li>
                     </Link>
                     <Link
-                    href={"/shop"}>
+                        href={"/shop"}>
                         <li className="text-white font-bold mx-4">Shop</li>
                     </Link>
                 </ul>
             </div>
-            
+
             <div className="">
-                <FacebookIcon className="text-white" />
-                <LocationOnIcon className="text-white" />
-                <TwitterIcon className="text-white" />
-                <InstagramIcon className="text-white" />
+                <div className="grid place-items-center w-[30px] h-[30px] bg-blue-600">
+                    <div><FacebookIcon className="text-black" /></div>
+                </div>
+                <div className="grid place-items-center w-[30px] h-[30px] bg-blue-600">
+                    <div><LocationOnIcon className="text-black" /></div>
+                </div>
+                <div className="grid place-items-center w-[30px] h-[30px] bg-blue-600">
+                    <div><TwitterIcon className="text-black" /></div>
+                </div>
+                <div className="grid place-items-center w-[30px] h-[30px] bg-blue-600">
+                    <div><InstagramIcon className="text-black" /></div>
+                </div>
             </div>
         </nav>
     )
